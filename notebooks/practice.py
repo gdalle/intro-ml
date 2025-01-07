e6473aec7cd12b1b8b272b5166a777a8a75c1419
Guillaume Dalle <22795598+gdalle@users.noreply.github.com>
2025-01-07 23:59:41 +0100

    Target fix

--- notebooks/practice.py
+++ notebooks/practice.py
@@ -227,7 +227,7 @@ def _(data, np, target):
     from sklearn.model_selection import train_test_split
 
     X_train, X_test, y_train, y_test = train_test_split(
-        data, np.bool(target), train_size=0.6, random_state=0
+        data, target, train_size=0.6, random_state=0
     )
     return X_test, X_train, train_test_split, y_test, y_train
 
